c8fef2749d2e6e5be5866fe1c593162b82daef6f
jocile <jocile@gmail.com>
2022-02-07 19:15:38 -0300

    Developed person create operation

--- src/main/java/one/digitalinnovation/personapi/controller/PersonController.java
+++ src/main/java/one/digitalinnovation/personapi/controller/PersonController.java
@@ -2,29 +2,28 @@ package one.digitalinnovation.personapi.controller;
 
 import one.digitalinnovation.personapi.dto.response.MessageResponseDTO;
 import one.digitalinnovation.personapi.entity.Person;
-import one.digitalinnovation.personapi.repository.PersonRepository;
+import one.digitalinnovation.personapi.service.PersonService;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseStatus;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 @RequestMapping("/api/v1/people")
 public class PersonController {
-  private PersonRepository personRepository;
+  private PersonService personService;
 
   @Autowired
-  public PersonController(PersonRepository personRepository) {
-    this.personRepository = personRepository;
+  public PersonController(PersonService personService) {
+    this.personService = personService;
   }
 
   @PostMapping
+  @ResponseStatus(HttpStatus.CREATED)
   public MessageResponseDTO cratePerson(@RequestBody Person person) {
-    Person savePerson = personRepository.save(person);
-    return MessageResponseDTO
-      .builder()
-      .message("Created person with id: " + savePerson.getId())
-      .build();
+    return personService.cratePerson(person);
   }
 }
---
+++ src/main/java/one/digitalinnovation/personapi/service/PersonService.java
@@ -0,0 +1,25 @@
+package one.digitalinnovation.personapi.service;
+
+import one.digitalinnovation.personapi.dto.response.MessageResponseDTO;
+import one.digitalinnovation.personapi.entity.Person;
+import one.digitalinnovation.personapi.repository.PersonRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+@Service
+public class PersonService {
+  private PersonRepository personRepository;
+
+  @Autowired
+  public PersonService(PersonRepository personRepository) {
+    this.personRepository = personRepository;
+  }
+
+  public MessageResponseDTO cratePerson(Person person) {
+    Person savedPerson = personRepository.save(person);
+    return MessageResponseDTO
+      .builder()
+      .message("Created person with id: " + savedPerson.getId())
+      .build();
+  }
+}
