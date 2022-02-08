030788396b97954a0b7e116a519c7653ecd484e1
jocile <jocile@gmail.com>
2022-02-07 23:30:14 -0300

    Refactory Lombok Constructor

--- src/main/java/one/digitalinnovation/personapi/service/PersonService.java
+++ src/main/java/one/digitalinnovation/personapi/service/PersonService.java
@@ -2,6 +2,7 @@ package one.digitalinnovation.personapi.service;
 
 import java.util.List;
 import java.util.stream.Collectors;
+import lombok.AllArgsConstructor;
 import one.digitalinnovation.personapi.dto.request.PersonDTO;
 import one.digitalinnovation.personapi.dto.response.MessageResponseDTO;
 import one.digitalinnovation.personapi.entity.Person;
@@ -12,15 +13,11 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 @Service
+@AllArgsConstructor(onConstructor = @__(@Autowired))
 public class PersonService {
   private PersonRepository personRepository;
 
   private final PersonMapper personMapper = PersonMapper.INSTANCE;
-
-  @Autowired
-  public PersonService(PersonRepository personRepository) {
-    this.personRepository = personRepository;
-  }
 
   public MessageResponseDTO createPerson(PersonDTO personDTO) {
     Person personToSave = personMapper.toModel(personDTO);
